8e6b0d279311db35018d00121d58c3e6b81a5eaf
ttekcor <curvhand@gmail.com>
2024-02-04 11:09:26 +1000

    Добавил админку с загрузкой файлов

--- bot.py
+++ bot.py
@@ -2,50 +2,102 @@ import telebot
 from telebot import types
 
 import pandas as pd
-from ts import slicer,separator,func_const,imagine
+from ts import slicer,imagine
 
 bot = telebot.TeleBot('5895426188:AAGeesw_OVKurBcmJaNv-aUJ46t0vwYhx7M')
 day_of_week = ""
+password = '!botcheck56'
+@bot.message_handler(commands=['start'])  
+def start(message):   
+        markup = types.ReplyKeyboardMarkup(row_width=3)
+        pn = types.InlineKeyboardButton('Понедельник',callback_data='pn')
+        vt = types.InlineKeyboardButton('Вторник',callback_data='vt')
+        sr = types.InlineKeyboardButton('Среда',callback_data='sr')
+        cht = types.InlineKeyboardButton('Четверг',callback_data='cht')
+        pt = types.InlineKeyboardButton('Пятница',callback_data='pt')
+        markup.add(pn,vt,sr,cht,pt)  
+        bot.send_message(message.chat.id,'День недели:',reply_markup=markup)
+        @bot.message_handler(content_types=['text'])  
+        def day(message):
+            global day_of_week  
+            day_of_week = message.text
+                
+        bot.register_next_step_handler(message, day)
+        bot.register_next_step_handler(message, button)
 
-@bot.message_handler(commands=['start'])  #реагирует на любые сообщения
-def start(message):
-        #Если содержимое == 'One',то 
+
+@bot.message_handler(commands=['admin']) 
+def admin(message):
+    bot.send_message(message.chat.id,'Введите пароль:')
+    @bot.message_handler(content_types=['text'])
+    def check(message):
+        if message.text == password:
+            bot.reply_to(message,"Успешно")
+            bot.reply_to(message,"Загрузите файл")
+            bot.register_next_step_handler(message,handle_docs_audio)
+            bot.register_next_step_handler(message,day_admin)
+        else:
+            bot.send_message(message.chat.id,'Неверный пароль')
+        
+    
+    bot.register_next_step_handler(message,check)
+
+
+@bot.message_handler(content_types=['text'])
+def day_admin(message):   
     markup = types.ReplyKeyboardMarkup(row_width=3)
     pn = types.InlineKeyboardButton('Понедельник',callback_data='pn')
     vt = types.InlineKeyboardButton('Вторник',callback_data='vt')
     sr = types.InlineKeyboardButton('Среда',callback_data='sr')
     cht = types.InlineKeyboardButton('Четверг',callback_data='cht')
     pt = types.InlineKeyboardButton('Пятница',callback_data='pt')
-    markup.add(pn,vt,sr,cht,pt)   #Bot reply 'Введите текст'
-    bot.send_message(message.chat.id,'День недели:',reply_markup=markup)
-    @bot.message_handler(content_types=['text'])  #Создаём новую функцию ,реагирующую на любое сообщение
-    def day(message):
-        global day_of_week  #объявляем глобальную переменную
-        day_of_week = message.text
-            
-        #print("!",message.text)
-    bot.register_next_step_handler(message, day)
-    bot.register_next_step_handler(message, button)
-# def day(message):
-#     markup = types.ReplyKeyboardMarkup(row_width=3)
-#     pn = types.InlineKeyboardButton('Понедельник',callback_data='pn')
-#     vt = types.InlineKeyboardButton('Вторник',callback_data='vt')
-#     sr = types.InlineKeyboardButton('Среда',callback_data='sr')
-#     cht = types.InlineKeyboardButton('Четверг',callback_data='cht')
-#     pt = types.InlineKeyboardButton('Пятница',callback_data='pt')
-#     markup.add(pn,vt,sr,cht,pt)
-#     print(message.text)
-#     bot.send_message(message.chat.id,'День недели:',reply_markup=markup)
-#     if message:
-#         global day_of_week 
-#         day_of_week = message.reply_markup
-        
+    markup.add(pn,vt,sr,cht,pt)  
+    bot.send_message(message.chat.id,'День недели:',reply_markup=markup)                
     
-  
-#@bot.callback_query_handler(func=lambda call:True)
-# def call_day(call):
-#     n = func_const(call.text)
-#     bot.register_next_step_handler(n, callback)
+    bot.register_next_step_handler(message,seq)
+
+
+@bot.message_handler(content_types=['document'])   
+def handle_docs_audio(message):
+    chat_id = message.chat.id
+    print(message.document)
+    global file_info
+    file_info = bot.get_file(message.document.file_id)
+     
+def seq(message):
+    global day_of_week
+    day_of_week = message.text
+    if message.text=="Понедельник":
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'.\sch_pn.xlsx'
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Пожалуй, я сохраню это")
+    elif message.text=="Вторник":
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'.\sch_vt.xlsx'
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Пожалуй, я сохраню это")
+    elif message.text=="Среда":
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'.\sch_sr.xlsx'
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Пожалуй, я сохраню это")
+    elif message.text=="Четверг":
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'.\sch_cht.xlsx'
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Пожалуй, я сохраню это")
+    elif message.text=="Пятница":
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'.\sch_pt.xlsx'
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Пожалуй, я сохраню это")
+    bot.register_next_step_handler(message,start)
 
 
 @bot.callback_query_handler(func=lambda message:True)
@@ -69,11 +121,6 @@ def button(message):
         markup.add(item7a,item7b,item7v,item7g,item8a,item8b,item8v,item8g,item9a,item9b,item9v,item10a,item10b,item11a)
         bot.send_message(message.chat.id,'Класс:',reply_markup=markup)
         bot.register_next_step_handler(message, callback)
-    
-        
-
-        
-    
     
 
 @bot.callback_query_handler(func=lambda call:True)
@@ -198,5 +245,6 @@ def callback(call):
         markup.add(bug)
         msg = bot.send_message(call.chat.id,"Заново?",reply_markup=markup)    
         bot.register_next_step_handler(msg, start)
+
 if __name__ =="__main__":
     bot.polling()    
