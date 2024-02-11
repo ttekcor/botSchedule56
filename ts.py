40f4c25755a5effc973aa36110891d0197854119
ttekcor <curvhand@gmail.com>
2024-02-12 00:04:10 +1000

    убрал ошибку повторения

--- ts.py
+++ ts.py
@@ -7,8 +7,8 @@ from numpy import nan
 
 a = ''
 password = 'Oksanakoren'
-token = '5895426188:AAGeesw_OVKurBcmJaNv-aUJ46t0vwYhx7M'#настоящий
-#token = '6779562330:AAE_d_QKj1n59lgJU_JT6s6PO4atUMlnUcM'
+#token = '5895426188:AAGeesw_OVKurBcmJaNv-aUJ46t0vwYhx7M'#настоящий
+token = '6779562330:AAE_d_QKj1n59lgJU_JT6s6PO4atUMlnUcM'
 def func_const(n):
     a = separator(n)
     return a
@@ -28,7 +28,7 @@ def slicer(n,path):
     
     new_op = pd.DataFrame(op, columns=["Урок",n])
     temp_op = new_op
-    
+    print(temp_op)
     result =[]
     for index in temp_op.index:
         y = temp_op['Урок'][index], temp_op[n][index]
@@ -65,5 +65,5 @@ def slicer_teach(n,path):
     return result_name
        
 
-#print(slicer_teach("Паранько","Понедельник"))
+#print(slicer("7А","Понедельник"))
 
