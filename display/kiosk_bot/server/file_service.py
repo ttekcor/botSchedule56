9e61623bdba6452bc05a75f0b3283d4730b9ab5e
Igor Shavlov <108840149+HarryShavlov@users.noreply.github.com>
2024-10-11 19:59:53 +0300

    that's back 0.1

---
+++ display/kiosk_bot/server/file_service.py
@@ -0,0 +1,17 @@
+import os
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'upload')
+
+# Убедимся, что папка для загрузки существует
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def save_file(file, file_name):
+    """Сохраняет файл на сервер."""
+    file_path = os.path.join(UPLOAD_FOLDER, file_name)
+    file.save(file_path)
+
+def file_exists(file_name):
+    """Проверяет, существует ли файл на сервере."""
+    return os.path.exists(os.path.join(UPLOAD_FOLDER, file_name))
+
